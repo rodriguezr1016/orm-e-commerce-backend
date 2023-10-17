7860b2f52e7235e780812d4e5865afe1b9bdf576
Rene Rodriguez <134916213+rodriguezr1016@users.noreply.github.com>
2023-10-16 20:14:01 -0700

    I worked on the index file inside the models folder because i had the wrong forein keys

--- models/index.js
+++ models/index.js
@@ -11,15 +11,17 @@ Product.belongsTo(Category, {
 
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: 'product_id'
+  foreignKey: 'category_id'
 });
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  foreignKey: 'tag_id'
+  through: ProductTag,
+  foreignKey: 'product_id'
 });
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  foreignKey: 'product_id'
+  through: ProductTag,
+  foreignKey: 'tag_id'
 });
 module.exports = {
   Product,
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
-const sequelize = require('sequelize');
+const sequelize = require('./config/connection');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
